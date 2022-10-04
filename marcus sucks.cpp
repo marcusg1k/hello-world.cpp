27372d03a6f0153d7ce4bc25971dd67f207daac7
marcusg1k <marcusgutierrezufc@gmail.com>
2022-10-04 16:19:15 -0500

    PUshing this code to github

---
+++ marcus sucks.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    cout << "This code is going to github" << endl;
+    cout << "This keyboard is not that bad for typing " << endl;
+    
+}
